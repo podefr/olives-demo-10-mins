215ac50c4bf30debf142f79b360035141145d358
Olivier Scherrer <pode.fr@gmail.com>
2013-01-20 21:41:27 +0000

    demo app

---
+++ cheatsheet/todo.js
@@ -0,0 +1,52 @@
+define(
+[
+	'LocalStore',
+	'OObject',
+	'Event.plugin',
+	'Bind.plugin'
+],
+
+function (Store, OObject, Event, Bind) {
+
+	 return function Demo(dom) {
+
+	 	var widget = new OObject();
+
+	 	var list = new Store([]);
+
+	 	var event = new Event({
+	 		addTask: function (event, node) {
+	 			if ( event.keyCode == 13 ) {
+	 				list.alter( 'push', node.value );
+	 				node.value = '';
+	 			}
+	 		},
+
+	 		removeTask: function (event, node) {
+	 			list.del( node.getAttribute('data-bind_id') );
+	 		}
+	 	});
+
+	 	var bind = new Bind(list, {
+	 		getId: function (item) {
+	 			this.innerHTML = list.alter("indexOf", item);
+	 		}
+	 	});
+
+	 	list.sync("todo-demo");
+
+	 	widget.plugins.addAll({
+	 		'event': event,
+	 		'bind': bind,
+	 		'custom': {
+	 			prepend: function (node, text) {
+	 				node.innerHTML = text + " " + node.innerHTML;
+	 			}
+	 		}
+	 	});
+
+	 	widget.alive(dom);
+
+	 };
+
+});
